df3dbc45373561b14c076ab62ee68db47afdeb9d
tinsin0314 <tinsin.chen@gmail.com>
2018-07-11 22:46:54 +0800

    按钮提交到里面

---
+++ src/app.js
@@ -0,0 +1,8 @@
+import Vue from 'vue'
+import GButton from './button'
+
+Vue.component('g-button',GButton);
+
+new Vue({
+    el:'#app',
+});
---
+++ src/button.vue
@@ -0,0 +1,29 @@
+<template>
+    <button class="g-button">按钮</button>
+</template>
+
+<script>
+    export default {
+    }
+</script>
+
+<style lang="scss">
+
+    .g-button{
+        padding:0 1em;
+        height:var(--botton-height);
+        border-color: var(--button-border-color);
+        border-radius: var(--button-border-redius);
+        font-size: var(--font-size);
+        &:hover{
+                border-color: var(--button-border-hover);
+        }
+        &:active{
+                border-color: var(--button-border-active);
+        }
+        &:focus{
+                outline: none;
+        }
+    }
+
+</style>
